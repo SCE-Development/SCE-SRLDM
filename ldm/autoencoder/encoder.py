01d29b57fe366c8be212c1f6ed363b31ea6a4fb8
chrehall68 <chrehall68@gmail.com>
2023-10-23 11:55:48 -0700

    add decoder, encoder, and way to get image comparisons

--- ldm/autoencoder/encoder.py
+++ ldm/autoencoder/encoder.py
@@ -1,19 +1,106 @@
-
-
+from typing import Tuple
 import torch
 import torch.nn as nn
+import numpy as np
+
+
+class EncoderUnit(nn.Module):
+    """
+    Encoder Unit; Halves the H,W of an input
+    """
+
+    def __init__(
+        self,
+        in_shape: Tuple[int, int, int],
+        out_shape: Tuple[int, int, int],
+        kernel_size: int,
+        pool_size: int,
+    ) -> None:
+        super(EncoderUnit, self).__init__()
+
+        # calculate necessary padding for the pool layer
+        # H out​ =floor( (H in​ +2×padding[0]−dilation[0]×(kernel_size[0]−1)−1​)/stride[0] + 1 )
+        stride = 2
+        dilation = 1
+        desired_height = out_shape[1]
+        height_without_padding = (
+            in_shape[1] + 2 * 0 - dilation * (pool_size - 1) - 1
+        ) // stride + 1
+        necessary_padding = (desired_height - height_without_padding) * 2 // stride
+
+        self.unit = nn.Sequential(
+            nn.Conv2d(
+                in_shape[-1],
+                out_shape[-1],
+                kernel_size=kernel_size,
+                stride=1,
+                padding="same",
+            ),
+            nn.GELU(),
+            nn.BatchNorm2d(out_shape[-1]),
+            nn.Conv2d(
+                out_shape[-1],
+                out_shape[-1],
+                kernel_size=kernel_size,
+                stride=1,
+                padding="same",
+            ),
+            nn.GELU(),
+            nn.MaxPool2d(kernel_size=pool_size, stride=2, padding=necessary_padding),
+        )
+
+    def forward(self, x: torch.Tensor):
+        return self.unit(x)
 
 
 class Encoder(nn.Module):
+    def __init__(
+        self,
+        inp_shape: Tuple[int, int, int],
+        out_shape: Tuple[int, int, int],
+        kernel_size: int,
+        pool_size: int,
+    ) -> None:
+        """
+        Initialize the encoder
 
-    def __init__(self):
-
+        Arguments:
+            - inp_shape: Tuple[int, int] - the H, W, C of the input images
+            - out_shape: Tuple[int, int] - the H, W, C of the output
+            - kernel_size: int - the size of the convolution kernels
+            - pool_size: int - the size of the MaxPool kernels
+        """
         super(Encoder, self).__init__()
 
-        return NotImplementedError
-    
+        cur_shape = np.array(inp_shape)
+        out_shape = np.array(out_shape)
+        i = 0
 
-    def forward(self, x):
+        # add downconv layers
+        while (cur_shape != out_shape).all():
+            desired_shape = np.array(
+                [cur_shape[0] // 2, cur_shape[1] // 2, out_shape[-1]]
+            )
+            if cur_shape[-1] * 2 <= desired_shape[-1]:
+                desired_shape[-1] = cur_shape[-1] * 2
+            self.add_module(
+                f"unit_{i}",
+                EncoderUnit(
+                    cur_shape,
+                    desired_shape,
+                    kernel_size=kernel_size,
+                    pool_size=pool_size,
+                ),
+            )
 
-        return NotImplementedError
-    
+            cur_shape = desired_shape
+            i += 1
+
+    def forward(self, x: torch.Tensor):
+        """
+        Call the model on B,C,H,W input
+        """
+        for module in self.children():
+            x = module(x)
+
+        return x
--- ldm/autoencoder/utils.py
+++ ldm/autoencoder/utils.py
@@ -1,3 +1,43 @@
-
-
 # utililty functions for the model go here
+import torch
+import random
+import matplotlib.pyplot as plt
+from PIL import Image
+
+
+def to_image(x: torch.Tensor):
+    return Image.fromarray(
+        (torch.clamp(x, 0, 1) * 255)
+        .permute(1, 2, 0)
+        .contiguous()
+        .to(torch.uint8)
+        .numpy()
+    )
+
+
+def get_comparison(num_comparisons: int, images: torch.Tensor, model: torch.nn.Module):
+    choices = []
+    while len(choices) != num_comparisons:
+        i = random.randint(0, len(images) - 1)
+        if i not in choices:
+            choices.append(i)
+
+    real_images = []
+    for i in choices:
+        real_images.append(to_image(images[i]))
+
+    reconstructed = torch.zeros((num_comparisons, 3, 32, 32))
+    for idx, ds_idx in enumerate(choices):
+        reconstructed[idx] = images[ds_idx]
+    reconstructed = reconstructed.to("cuda")
+    reconstructed = model(reconstructed).detach().cpu()
+
+    reconstructed_imgs = []
+    for i in range(num_comparisons):
+        reconstructed_imgs.append(to_image(reconstructed[i]))
+
+    _, axis_arr = plt.subplots(num_comparisons, 2)
+    for i in range(num_comparisons):
+        axis_arr[i, 0].imshow(real_images[i])
+        axis_arr[i, 1].imshow(reconstructed_imgs[i])
+    plt.savefig(fname="comparisons")
